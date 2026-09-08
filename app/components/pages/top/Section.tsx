fcf839bc8f3eaed3a73097fa4b4ed988de8299ef
neila <40727091+neila@users.noreply.github.com>
2026-09-08 01:17:29 -0700

    a little wider

--- app/components/pages/top/Section.tsx
+++ app/components/pages/top/Section.tsx
@@ -10,7 +10,7 @@ const Section = ({
   className?: string;
 }) => (
   <section class={`w-full pt-8 pb-9 px-6 ${className}`}>
-    <div class="max-w-3xl mx-auto">
+    <div class="max-w-4xl mx-auto">
       {title ? (
         <h3 class="font-bold text-center text-2xl pb-5">{title}</h3>
       ) : null}
